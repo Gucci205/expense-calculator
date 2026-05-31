166fa6e8d9ce83ce016058aa4742d36d39216b5d
Gucci205 <lwinmoeaung20005@gmail.com>
2026-05-31 17:39:01 +0700

    update the calculation

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ const getAmounts = document.querySelectorAll('.amount');
 const getSpentAmount = document.querySelectorAll('.spent-amount');   //when calling an element with querySelectorAll, you cant get the value of that element, just querySelector works fine
 const getRemainingAmount = document.querySelectorAll('.remaining-amount');
 
-// console.log(getAmountInputValue);
-// console.log(getAmounts);
-// console.log(getSpentAmount);
-console.log(getRemainingAmount);
+budgetAllocation();
 
 function budgetAllocation(){
     const numberCasting = Number(getAmountInputValue);
-    const needs = numberCasting * 0.5;
-
+    let needs = numberCasting * 0.5;
+    // console.log(needs);
     const wants = numberCasting * 0.3;
 
     const saving = numberCasting * 0.2;
@@ -23,17 +20,30 @@ function budgetAllocation(){
         getAmount.innerHTML = arrayOfAmounts[index]; //placing amount to respective p tag
         getAmount.style.fontFamily = 'Times New Roman';
         
-        console.log(arrayOfAmounts[index] - getSpentAmount[index].value);
-        
-        console.log(getRemainingAmount[index].value);
+        // console.log(arrayOfAmounts[index] - getSpentAmount[index].value);
+
         getRemainingAmount[index].value = arrayOfAmounts[index] - getSpentAmount[index].value;
     });
+
+    let need = String(needs);
+    console.log(need);
+    let arr = [];
+    for(num of need){
+        // console.log(num);
+        arr.push(num);
+    }
+    arr.reverse();
+    let str = '';
+    let count = 0;
+    for(let i = 0; i < arr.length; i++){
+        // console.log(arr[i]);
+        str+= arr[i];
+        count++;
+        if(count % 3 === 0){
+            str+= ',';
+        }
+    }
+    console.log(arr);
+    
+    console.log(str);
 }
-
-// function calculation(){
-//     console.log(needs);
-// }
-
-
-budgetAllocation();
-// calculation();
