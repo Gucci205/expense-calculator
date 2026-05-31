47ab18620ce00025983d62d4b6b422d36355ac25
Gucci205 <lwinmoeaung20005@gmail.com>
2026-05-31 14:25:53 +0700

    update the calculation

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const getAmountInputValue = document.querySelector('.amountInput').value;
 const getAmounts = document.querySelectorAll('.amount');
-const getSpentAmount = document.querySelectorAll('.spentAmount');
+const getSpentAmount = document.querySelectorAll('.spent-amount');   //when calling an element with querySelectorAll, you cant get the value of that element, just querySelector works fine
+const getRemainingAmount = document.querySelectorAll('.remaining-amount');
 
-console.log(getSpentAmount);
+// console.log(getAmountInputValue);
+// console.log(getAmounts);
+// console.log(getSpentAmount);
+console.log(getRemainingAmount);
 
 function budgetAllocation(){
     const numberCasting = Number(getAmountInputValue);
@@ -15,15 +19,21 @@ function budgetAllocation(){
     const arrayOfAmounts = [needs, wants, saving];
     // console.log(arrayOfAmounts);
 
-    getAmounts.forEach((amount, index) => {
-        amount.innerHTML = arrayOfAmounts[index];
-        amount.style.fontFamily = 'Times New Roman'
-    })
+    getAmounts.forEach((getAmount, index) => {
+        getAmount.innerHTML = arrayOfAmounts[index]; //placing amount to respective p tag
+        getAmount.style.fontFamily = 'Times New Roman';
+        
+        console.log(arrayOfAmounts[index] - getSpentAmount[index].value);
+        
+        console.log(getRemainingAmount[index].value);
+        getRemainingAmount[index].value = arrayOfAmounts[index] - getSpentAmount[index].value;
+    });
 }
 
-function calculation(){
-}
+// function calculation(){
+//     console.log(needs);
+// }
 
 
 budgetAllocation();
-calculation();
+// calculation();
