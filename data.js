38938786265a97a4e09a92178004aca8c3d4b1db
Gucci205 <lwinmoeaung20005@gmail.com>
2026-09-25 19:43:16 +0700

    Update spent amount and create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const data = [
+    {
+        category: 'need',
+        expense: [],
+        remaining:[]
+    },
+    {
+        category: 'want',
+        expense: [],
+        remaining:[]
+    },
+    {
+        category: 'save',
+        expense: [],
+        remaining:[]
+    }
+];
